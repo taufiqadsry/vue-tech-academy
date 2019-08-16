5459642e0da9bb5af503d9ffe4a2b0d8815bf6a4
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-08-16 16:40:31 +0700

    add user form  component

---
+++ src/components/UserView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="user-view">
+        <div class="row">
+            <div class="col-md-3">
+                <img :src="user.avatar" alt="failed">
+            </div>
+            <div class="col-md-9">
+                <h5 v-text="user.first_name + ' ' + user.last_name"></h5>
+                <p>{{ user.email }} -  {{ user.job ? user.job : 'Nganggur' }}</p>
+            </div>
+        </div>    
+    </div>
+</template>
+
+<style>
+    .user-view img {
+        width: 50px;
+        height: 50px;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
