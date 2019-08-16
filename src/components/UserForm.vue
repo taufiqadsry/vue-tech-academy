d6fa309412b89f22fb1a8b1d1870ea6016480e69
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-08-16 16:40:08 +0700

    add user form  component

---
+++ src/components/UserForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <form :class="name" class="form-inline" @submit.prevent="submit" @keypress.enter="submit">
+        <input class="form-control" placeholder="name" :disabled="loading" type="text" v-model="name">
+        <input class="form-control" placeholder="job" :disabled="loading" type="text" v-model="job">
+        <input class="btn btn-primary" :disabled="loading" type="submit" value="Add">
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'user-form',
+    props: {
+        loading: {
+            type: Boolean
+        }
+    },
+    
+    data() {
+        return {
+            name: '',
+            job: '',
+        }
+    },
+
+    methods: {
+        submit() {
+            this.$emit('add-new-user', { name: this.name, job: this.job })
+        }
+    }
+}
+</script>
