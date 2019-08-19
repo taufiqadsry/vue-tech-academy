559c208084bb72b4f028edd64245d21f478e6265
rudirahardian <rudi.rahardian@ralali.com>
2019-08-19 11:21:29 +0700

    create, read, delete

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <UsersTable title="Users List"/>
-  </div>
+    <div id="app">
+        <UsersTable title="Users List"/>
+    </div>
 </template>
 
 <script>
-import UsersTable from "./components/Users";
+    import UsersTable from "./components/Users";
 
-export default {
-  name: 'app',
-  components: {
-    UsersTable
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            UsersTable
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,35 +1,68 @@
 <template>
     <div class="row">
-        <h2> {{ title }} </h2>
+        <div>
+            <h2> {{ title }} </h2>
+            <form @submit="add" @keypress.enter="add" class="form">
+                <input type="text" v-model="name" placeholder="name">
+                <input type="text" v-model="job" placeholder="job">
+                <button>Add</button>
+            </form>
+        </div>
+
         <table class="table">
             <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">avatar</th>
-                    <th scope="col">email</th>
-                    <th scope="col">fullname</th>
-                    <th scope="col">action</th>
-                </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">avatar</th>
+                <th scope="col">email</th>
+                <th scope="col">fullname</th>
+                <th scope="col">action</th>
+            </tr>
             </thead>
             <tbody>
-                <tr v-for="(dt, key) in users.data">
-                    <th scope="row">{{ key }}</th>
-                    <td><img :src="dt.avatar"/></td>
-                    <td>{{dt.email}}</td>
-                    <td>{{dt.first_name + " " +dt.last_name}}</td>
-                    <td>
-                        <a href="#">delete</a>
-                        <a href="#">edit</a>
-                        <a href="#">view</a>
-                    </td>
-                </tr>
+            <tr v-for="(dt, key) in users.data">
+                <th scope="row">
+                    {{ key }}
+
+                </th>
+                <td><img :src="dt.avatar" class="ava"/></td>
+                <td>{{dt.email}}</td>
+                <td>{{dt.first_name + " " +dt.last_name}}</td>
+                <td>
+                    <a href="#" @click="hapus(key)" >delete</a>
+                    <a href="#">edit</a>
+                    <a href="#">view</a>
+                </td>
+            </tr>
             </tbody>
         </table>
     </div>
 </template>
 
 <style>
-    tbody a { padding: 5px;}
+    .row {
+        margin-top: -35px;
+    }
+
+    tbody a {
+        padding: 5px;
+    }
+
+    h2 {
+        text-align: left;
+    }
+
+    .form {
+        float: right;
+        text-align: left;
+        padding-top: 10px;
+        padding-bottom: 30px;
+    }
+
+    .ava {
+        height: 128px;
+        width: 128px;
+    }
 </style>
 
 <script>
@@ -47,29 +80,52 @@
         },
         data() {
             return {
-                users: []
+                users: [],
+                name: '',
+                job: ''
             }
         },
         created() {
             // axios get list here
-            this.load()
+            this.load();
         },
         methods: {
             load() {
-                axios.get(config.users_url).then(res => {
-                    this.users = res.data 
-                }).catch ((err) => {
-                    console.log(err);
-                })
+                axios
+                    .get(config.users_url)
+                    .then(response => {
+                        this.users = response.data
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
             },
-            add() {
-                
+            add(e) {
+                e.preventDefault();
+                axios
+                    .post(config.users_url)
+                    .then(res => {
+                        res.data.avatar = require('@/assets/ward.jpeg');
+                        res.data.email = '*Need Verification';
+                        this.users.data.push(res.data);
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
             },
-            delete(id) {
-                
+            hapus(index) {
+                axios
+                    .delete(config.users_url+ '/' + this.users.data[index].id)
+                    .then(res => {
+                        this.users.data.splice(index, 1)
+                        console.log(this.users.data)
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
             },
             edit(id) {
-                
+
             },
             view(id) {
 
